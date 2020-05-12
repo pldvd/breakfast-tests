c076d9c851c851588067bb7dd962b88e4e2f705e
David Pal <paldavid2030@gmail.com>
2020-05-12 09:52:46 +0300

    Add tests for iterables too

--- day-01/index.js
+++ day-01/index.js
@@ -6,5 +6,15 @@ function addFiveConditional(num) {
   return num % 2 === 0 ? num + 5 : num + 3;
 }
 
+function sayHi(name) {
+  return name ? `Hi ${name}!` : 'Hello, Stranger';
+}
+
+function pushToArray(array, item) {
+  array.push(item);
+}
+
 exports.addFive = addFive;
 exports.addFiveConditional = addFiveConditional;
+exports.sayHi = sayHi;
+exports.pushToArray = pushToArray;
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,6 +1,8 @@
 const dayOneMethods = require('./index');
 const addFive = dayOneMethods.addFive;
 const addFiveConditional = dayOneMethods.addFiveConditional;
+const sayHi = dayOneMethods.sayHi;
+const pushToArray = dayOneMethods.pushToArray;
 
 describe('addFive', () => {
   it('adds five', () => {
@@ -23,5 +25,50 @@ describe('addFiveConditional', () => {
 
   it('adds three to odd input', () => {
     expect(addFiveConditional(3)).toBe(6);
+  })
+
+  it('returns a value', () => {
+    expect(addFiveConditional(2)).not.toBeUndefined();
+  })
+})
+
+describe('sayHi', () => {
+  it('greets a persons with his/her name, if name is known', () => {
+    expect(sayHi('David')).toMatch(/hi david!/i)
+  })
+
+  it('greets a person with unknown name as "stranger"', () => {
+    expect(sayHi()).toMatch(/stranger/i);
+  })
+})
+
+describe('pushToArray', () => {
+  it('adds items to an array and returns the right length', () => {
+    const myArray = [];
+    pushToArray(myArray, 1);
+    pushToArray(myArray, 2);
+    pushToArray(myArray, 3);
+
+    expect(myArray).toHaveLength(3);
+  })
+
+  it('adds items to an array and returns the right members', () => {
+    const myArray = [];
+    pushToArray(myArray, 1);
+    pushToArray(myArray, 2);
+    pushToArray(myArray, 3);
+
+    expect(myArray).toEqual([1, 2, 3]);
+  })
+
+  it('adds items to an array and contains the right members', () => {
+    const myArray = [];
+    pushToArray(myArray, 'bread');
+    pushToArray(myArray, 'salad');
+    pushToArray(myArray, 'pasta');
+
+    expect(myArray).toContain('bread');
+    expect(myArray).toContain('pasta');
+    expect(myArray).toContain('salad');
   })
 })
