718f4136f02f1ff5dfa66916364c1af090f8fe5e
David Pal <paldavid2030@gmail.com>
2020-05-12 09:18:14 +0300

    Add second test also testing numbers

--- day-01/index.js
+++ day-01/index.js
@@ -2,4 +2,9 @@ function addFive(num) {
   return num + 5;
 }
 
+function addFiveConditional(num) {
+  return num % 2 === 0 ? num + 5 : num + 3;
+}
+
 exports.addFive = addFive;
+exports.addFiveConditional = addFiveConditional;
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,8 +1,19 @@
 const dayOneMethods = require('./index');
 const addFive = dayOneMethods.addFive;
+const addFiveConditional = dayOneMethods.addFiveConditional;
 
 describe('addFive', () => {
   it('adds five', () => {
     expect(addFive(6)).toEqual(11);
+  })
+})
+
+describe('addFiveConditional', () => {
+  it('adds five to even input', () => {
+    expect(addFiveConditional(2)).toBe(7);
+  })
+
+  it('adds three to odd input', () => {
+    expect(addFiveConditional(3)).toBe(6);
   })
 })
