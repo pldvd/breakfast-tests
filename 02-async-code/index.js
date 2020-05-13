e7d593328120cb01c8bd2365b6794349db514944
David Pal <paldavid2030@gmail.com>
2020-05-13 14:18:12 +0300

    Add a custom promise and tests for both resolve and reject cases

--- 02-async-code/index.js
+++ 02-async-code/index.js
@@ -4,4 +4,15 @@ function getResource(url) {
   return axios.get(url).then(response => response.data);
 }
 
+function myPromise(input) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (input % 2 === 0) {
+        resolve('the number is even');
+      } else reject('this is an odd number');
+    }, 3000)
+  })
+}
+
 exports.getResource = getResource;
+exports.myPromise = myPromise;
--- 02-async-code/index.test.js
+++ 02-async-code/index.test.js
@@ -1,10 +1,24 @@
 const asyncFunctions = require('./index.js');
 const getResource = asyncFunctions.getResource;
+const myPromise = asyncFunctions.myPromise;
 
 describe('getResource', () => {
-  
   it('fetches the users array', () => {
     return getResource('https://jsonplaceholder.typicode.com/users')
       .then(data => expect(data).toHaveLength(10));
+  })
+})
+
+describe('myPromise', () => {
+  it('resolves with "the number is even" if even input is passed', () => {
+    return myPromise(2).then(message => {
+      expect(message).toMatch(/even/i);
+    })
+  })
+
+  it('rejects with reason "this is an odd number" if odd input is passed', () => {
+    return myPromise(3).catch(reason => {
+      expect(reason).toMatch(/odd/i);
+    })
   })
 })
